45ebc127c2117705f8d67beead2f7945956b14fa
Evan <90571934+evankrieger@users.noreply.github.com>
2025-03-20 01:22:30 -0500

    Initial commit

---
+++ src/main/java/com/example/cbdatabase/CbDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.cbdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cbdatabase/CbDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cbdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
