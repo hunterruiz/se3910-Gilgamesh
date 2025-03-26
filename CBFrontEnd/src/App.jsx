ce275582fa2d03d99d515e9e8a5710e621fdf585
Evan <90571934+nyaphilim@users.noreply.github.com>
2025-03-26 16:40:26 -0500

    ReactJS Part 1
    Started the front end and completed ReactJS lesson 1

---
+++ CBFrontEnd/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Header/>
+        Hello ReactJS
+      </div>
+    </>
+  )
+}
+
+export default App
