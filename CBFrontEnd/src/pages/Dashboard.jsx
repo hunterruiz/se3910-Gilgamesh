39f6ad15a5e275e6bce2eb0860a85f02f47563a1
Anton Fleming <aflemingrocks089@gmail.com>
2025-03-28 14:42:00 -0500

    Able to get status code and headers from URL

--- CBFrontEnd/src/pages/Dashboard.jsx
+++ CBFrontEnd/src/pages/Dashboard.jsx
@@ -1,35 +1,58 @@
 import { useState } from "react";
 import { useEffect } from "react";
 
+// can use this free api for testing https://jsonplaceholder.typicode.com/todos
+// place url into URL search on browser
 function Dashboard() {
-  // GET statement that retrieves the html file for the URL imputed
-  function useFetch(event) {
-    event.preventDefault();
-
-    // TO DO gets url but returns the HTML file
-    fetch(URL, {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(),
-    })
-      .then((response) => {
-        response.json();
-      })
-      .then((data) => console.log(data))
-      .catch((error) => console.error("Error: ", error));
-  }
-
   // use state to hold state of the url
   const [url, setUrl] = useState("");
+  const [httpsReq, setHttpsReq] = useState({
+    url: "",
+    certificates: {},
+    status: null,
+    headers: {},
+    protocol: null,
+  });
+
+  // TO DO need to fix CORS error
+  function useFetch(event) {
+    event.preventDefault();
+    // TO DO add url validation
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response;
+      })
+      .then((data) => {
+        console.log("Raw data\n");
+        console.log(data);
+        setHttpsReq({
+          url: data.url,
+          // TO DO figure out how to get certificate
+          certificates: {},
+          status: data.status,
+          headers: data.headers,
+          // TO DO figure out how to get protocol
+          protocol: null,
+        });
+        console.log("httpReq state\n");
+        console.log(httpsReq);
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  }
 
   return (
     // html elements that are returned in the browser
     <>
       <div>
         <form onSubmit={useFetch}>
-          <label htmlFor="url-search">Url Search</label>
+          <label htmlFor="url-search">URL Search</label>
           <input
-            autocomplete="off"
+            autoComplete="off"
             id="url-search"
             placeholder="commercebank.com"
             onChange={(e) => setUrl(e.target.value)}></input>
