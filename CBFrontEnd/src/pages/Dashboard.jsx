6ae30c1c85646a93ac364133b0a686450061cede
Anton Fleming <aflemingrocks089@gmail.com>
2025-03-28 14:58:47 -0500

    Added comments and setup AnalysisTable.jsx to recive props from Dashboard.jsx

--- CBFrontEnd/src/pages/Dashboard.jsx
+++ CBFrontEnd/src/pages/Dashboard.jsx
@@ -15,6 +15,9 @@ function Dashboard() {
   });
 
   // TO DO need to fix CORS error
+  // runs after clicking Fetch button in browser
+  // might need to hit fetch twice because it only outputs the current
+  // response the second time
   function useFetch(event) {
     event.preventDefault();
     // TO DO add url validation
@@ -38,6 +41,9 @@ function Dashboard() {
           protocol: null,
         });
         console.log("httpReq state\n");
+        // do fn-f12 or f12 to see this response in the browser
+        // this opens the console cmd in browser.
+        // click on console and the messages
         console.log(httpsReq);
       })
       .catch((error) => {
@@ -49,6 +55,7 @@ function Dashboard() {
     // html elements that are returned in the browser
     <>
       <div>
+        {/* onSumbit runs the useFetch. useFetch is at the top of this file */}
         <form onSubmit={useFetch}>
           <label htmlFor="url-search">URL Search</label>
           <input
