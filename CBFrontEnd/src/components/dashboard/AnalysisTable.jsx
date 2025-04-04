e69754bfa95b1a4a7b5a481556924a53e54c0883
Anton Fleming <aflemingrocks089@gmail.com>
2025-04-03 20:45:04 -0500

    Made it so the actual headers apear in the fetch statment and set up some html in AnalysisTable so the responses JSON from fetch dashboard looks good and originized when rendered on frontend

--- CBFrontEnd/src/components/dashboard/AnalysisTable.jsx
+++ CBFrontEnd/src/components/dashboard/AnalysisTable.jsx
@@ -1,5 +1,6 @@
 import Table from "react-bootstrap/Table";
 import styles from "../../styles/analysis-table.module.css";
+import ListGroup from "react-bootstrap/ListGroup";
 
 // TO DO Take data from another component and pass it to this one.
 // documentation on how to do this https://react.dev/learn/passing-props-to-a-component
@@ -8,32 +9,50 @@ function AnalysisTable() {
     <div className={styles.wrapper}>
       {/* TO DO add backend url to action */}
       <form method="POST" action="backend-url">
-        <div>
-          <label htmlFor="url">URL:</label>
-          <input name="url" value="www.google.com" />
-
-          <button className={styles.saveButton} type="submit">
-            SAVE
-          </button>
-        </div>
-
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>URL</th>
-              <th>Last Scanned</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Google Home page</td>
-              <td>https://www.google.com/</td>
-              <td>3/27/2025</td>
-            </tr>
-          </tbody>
-        </Table>
+        {/* TO DO send data to backend. */}
       </form>
+      <div>
+        <h2>URL:</h2>
+        <h3>www.google.com</h3>
+      </div>
+
+      <div>
+        <h6>Last Scanned: </h6>
+        <p>11/20/2025</p>
+      </div>
+
+      <Table name="sll-certificate" striped bordered hover>
+        <thead>
+          <tr>
+            <th>SSL Certificate</th>
+            <th>Expiration</th>
+            <th>Protocol</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>code</td>
+            <td>date</td>
+            <td>https</td>
+            <td>200</td>
+          </tr>
+        </tbody>
+      </Table>
+      <Table striped bordered hover>
+        <thead>
+          <tr></tr>
+        </thead>
+        <tbody>
+          <tr></tr>
+        </tbody>
+      </Table>
+
+      <h2>Headers</h2>
+      {/* TO DO make overflow hidden */}
+      <ListGroup>
+        <ListGroup.Item>cache-control: "no-chache"</ListGroup.Item>
+      </ListGroup>
     </div>
   );
 }
