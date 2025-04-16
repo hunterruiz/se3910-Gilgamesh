45d94f76aaa8142e109ccfdc80807145a1764955
Jlc81850 <Jlc81850@ucmo.edu>
2025-04-16 16:38:31 -0500

    login connected to backend
    a little changed

--- CBFrontEnd/src/pages/Login.jsx
+++ CBFrontEnd/src/pages/Login.jsx
@@ -5,14 +5,14 @@ function Login() {
 
     return (
         <div>
-         <Form>
-            <Form.Group className="mb-3" controlId="formGroupEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+         <Form method="postmapping" action="/user">
+            <Form.Group className="mb-3" controlId="formGroupUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="text" name="Name" placeholder="Enter username" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formGroupPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control type="password" name="password" placeholder="Password" />
             </Form.Group>
             <Button variant="primary" type="submit">
                 Login
