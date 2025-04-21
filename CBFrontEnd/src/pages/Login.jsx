73a89e3e38367ca3ed1fd54a129ea75384b2e8a3
Jlc81850 <Jlc81850@ucmo.edu>
2025-04-21 16:57:27 -0500

    Attempt to Connect Front to Back
    Back end will create a user but the name and password will not be saved.

--- CBFrontEnd/src/pages/Login.jsx
+++ CBFrontEnd/src/pages/Login.jsx
@@ -1,25 +1,77 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import {useState} from "react";
+import {useNavigate} from 'react-router-dom';
 
 
 function Login() {
 
-    return (
+ const[user, setUser] = useState({
+        accountName:'',
+        accountPassword:'',
+      });
+
+    const changeValue=(e)=>{
+      console.log(e);
+      setUser;({
+        ...user,[e.target.name]:e.target.value
+      });
+      console.log(e.target.name + "name");
+      console.log(e.target.value + "value");
+    }
+
+    const navigate = useNavigate();  
+
+
+    const submitUser =(e)=>{
+        e.preventDefault();
+        fetch("http://localhost:8080/user", {
+          method:"POST",
+          headers:{
+            "Content-Type" : "application/json"
+          },
+          body: JSON.stringify(user)
+        })
+        .then(res=>{
+            console.log(1,res);
+            if(res.status === 201){
+              return res.json();
+            }else{
+              return null;
+            }
+          })
+        .then(res=>{
+          console.log(res)
+          if(res!==null){
+            navigate("/saved");
+          }else{
+            alert('fails');
+          }
+        
+        });
+    
+    }  
+
+      return (
         <div>
-         <Form method="postmapping" action="/h2-console/user">
-            <Form.Group className="mb-3" controlId="formGroupUsername">
+         <Form onSubmit = {submitUser}>
+            <Form.Group className="mb-3" controlId="formBasicUsername">
               <Form.Label>Username</Form.Label>
-              <Form.Control type="text" name="accountName" placeholder="Enter username" />
+              <Form.Control type="text"  placeholder="Enter username" name="accountName" onChange = {changeValue}  />
+              <Form.Text className="text-muted">
+              </Form.Text>
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formGroupPassword">
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" name="accountPassword" placeholder="Password" />
+              <Form.Control type="password"  placeholder="Password" name="accountPassword" onChange = {changeValue} />
             </Form.Group>
+
             <Button variant="primary" type="submit">
                 Login
             </Button> 
           </Form>
-          <a href="./pages/SignUp">Sign Up</a>
+          {/* <a href="./pages/SignUp">Sign Up</a> */}
         </div>
       );
  
--- CBFrontEnd/src/pages/SignUp.jsx
+++ CBFrontEnd/src/pages/SignUp.jsx
@@ -7,19 +7,7 @@ function SignUp() {
 
     return (
         <div>
-         <Form >
-            <Form.Group className="mb-3" controlId="formGroupUsername">
-              <Form.Label>Create Username</Form.Label>
-              <Form.Control type="text" placeholder="Enter username" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formGroupPassword">
-              <Form.Label>Create Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Sign Up
-            </Button> 
-          </Form>
+           SignUp
         </div>
       );
  
