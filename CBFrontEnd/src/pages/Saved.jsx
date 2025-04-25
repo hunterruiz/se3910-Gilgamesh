337eace4e4570d9d2699b810c46a8414669d902c
Anton Fleming <aflemingrocks089@gmail.com>
2025-04-25 15:47:00 -0500

    Created View page
    Created view page where url infomation can be displayed after clicking on the url from the list. Moved commanly used fetch functions to fetch.js

--- CBFrontEnd/src/pages/Saved.jsx
+++ CBFrontEnd/src/pages/Saved.jsx
@@ -1,38 +1,44 @@
 import Table from "react-bootstrap/Table";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 
 function Saved() {
   const [urls, setUrls] = useState([]);
+  const navigate = useNavigate();
 
-  useEffect(()=>{
-      fetch("http://localhost:8080/urls", {method:"GET"})
-      .then(res =>res.json())
-      .then(res=>{
-          setUrls(res)});
-  },[])
+  useEffect(() => {
+    fetch("http://localhost:8080/urls", { method: "GET" })
+      .then((res) => res.json())
+      .then((res) => {
+        setUrls(res);
+      });
+  }, []);
 
   return (
     <Table striped bordered hover>
-    <thead>
+      <thead>
         <tr>
-        <th>#</th>
-        <th>Url Name</th>
-        <th>Url</th>
+          <th>id</th>
+          <th>Name</th>
+          <th>URL</th>
         </tr>
-    </thead>
-    <tbody>
-
-    {urls.map(url =>
-
-        <tr key = {url.url_id}>
-        <td>{url.url_id}</td>
-        <td>{url.name}</td>
-        <td>{url.url}</td>
-        </tr>
-        )}
-
-    </tbody>
-    </Table> 
+      </thead>
+      <tbody>
+        {urls.map((url) => (
+          <tr
+            onClick={() => {
+              navigate("/saved/view/" + url.urlId, {
+                state: { name: url.name, url: url.url, urlId: url.urlId },
+              });
+            }}
+            key={url.urlId}>
+            <td>{url.urlId}</td>
+            <td>{url.name}</td>
+            <td>{url.url}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
 }
 
---
+++ CBFrontEnd/src/pages/View.jsx
@@ -0,0 +1,74 @@
+import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { AnalysisTableNoSave } from "../components/dashboard/AnalysisTable";
+import { scanUrl, saveUrl, deleteUrl } from "../utilities/fetch";
+import Button from "react-bootstrap/Button";
+
+function View() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const [url, setUrl] = useState({
+    urlId: location.state.urlId,
+    url: location.state.url,
+    name: location.state.name,
+  });
+
+  const [httpsRes, setHttpsRes] = useState({
+    url: url.url,
+    certificate: {},
+    status: null,
+    headers: {},
+    protocol: null,
+  });
+
+  function routeToSaved(res) {
+    if (res !== null) {
+      navigate("/saved");
+    } else {
+      alert("fails");
+    }
+  }
+
+  return (
+    <>
+      <div>
+        {" "}
+        <Button
+          onClick={() => {
+            const res = saveUrl(url);
+            routeToSaved(res);
+          }}
+          variant="success">
+          Update
+        </Button>
+        <Button
+          onClick={() => {
+            scanUrl(url, setHttpsRes);
+          }}
+          variant="success">
+          Rescan
+        </Button>
+        <Button
+          onClick={() => {
+            const res = deleteUrl(url);
+            routeToSaved(res);
+          }}
+          variant="danger">
+          Delete
+        </Button>
+      </div>
+      <div>
+        <label htmlFor="url-name">Name</label>
+        <input
+          id="url-name"
+          value={url.name}
+          onChange={(e) => setUrl({ ...url, name: e.target.value })}></input>
+      </div>
+      <AnalysisTableNoSave httpsRes={httpsRes} />
+    </>
+  );
+}
+
+export default View;
